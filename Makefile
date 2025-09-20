489ddb4ee573b8183fa594412152b8326f211081
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-09-20 13:21:03 -0300

    feat: hello world golang
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

---
+++ Makefile
@@ -0,0 +1,3 @@
+# TODO(fabri): emprolijar
+build-server:
+	cd system; go run main.go
---
+++ system/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"system/rabbit"
+)
+
+func main() {
+    fmt.Println("hello world")
+}
