548d39509a5bc32eec2c821f49ea47e4e76fcb13
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-09-21 20:54:49 -0300

    feat(WIP): client
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- client/main.go
+++ client/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
+	// "malasian_coffe/packet"
 )
 
+func createPackagesFrom(dir string) {
+}
+
 func main() {
+	// Directory with all dataset subdirectories
+	dataset_directory := os.Args[1]
 	fmt.Println("hello world")
 }
