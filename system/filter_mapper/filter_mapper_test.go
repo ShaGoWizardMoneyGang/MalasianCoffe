13178472d66904d6b39c1f21d8c0d9ca3371242d
Bruno Starnone <bstarnone@fi.uba.ar>
2025-09-22 19:43:44 -0300

    dejame al día

---
+++ system/filter_mapper/filter_mapper_test.go
@@ -0,0 +1,16 @@
+package filter_mapper
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFilterMapper(t *Testing) {
+	//data = bytes[]
+	//header = basura
+	//Packet {data + bytes}
+	//filterMapperOptions
+	//worker = filterMaper.new(options)
+	//result = worker(Packet)
+	//result =! expected => panic!
+}
