655227b662700a301640a3823998d121189adc4f
Bruno Starnone <bstarnone@fi.uba.ar>
2025-09-22 22:13:29 -0300

    mapper filter filtra por año entre 2024 y 2025

--- system/filter_mapper/filter_mapper_test.go
+++ system/filter_mapper/filter_mapper_test.go
@@ -2,15 +2,33 @@ package filter_mapper
 
 import (
 	"fmt"
+	"malasian_coffe/packet"
 	"testing"
 )
 
-func TestFilterMapper(t *Testing) {
-	//data = bytes[]
-	//header = basura
-	//Packet {data + bytes}
-	//filterMapperOptions
-	//worker = filterMaper.new(options)
-	//result = worker(Packet)
-	//result =! expected => panic!
+func TestFilterMapper(t *testing.T) {
+	// Mock data for testing
+	data := []byte("2ae6d188-76c2-4095-b861-ab97d3cd9312,4,5,,,38.0,0.0,38.0,2024-07-01 07:00:00\n7d0a474d-62f4-442a-96b6-a5df2bda8832,7,1,,,33.0,0.0,33.0,2025-07-01 07:00:02")
+	fmt.Println("Data to be processed: \n", string(data))
+	// Create a Packet instance
+	pkt := packet.Packet{
+		Payload: data,
+	}
+
+	// Create a new worker using the filter mapper options
+	worker := FilterMapper{
+		Function: filterFunction1,
+	}
+
+	// Process the packet using the worker
+	result := string(worker.Process(pkt).Payload)
+	fmt.Println("Processed result: \n", result)
+
+	// Validate the result
+	expected := packet.Packet{
+		Payload: []byte("2ae6d188-76c2-4095-b861-ab97d3cd9312,38.0\n7d0a474d-62f4-442a-96b6-a5df2bda8832,33.0\n"),
+	}
+	if result != string(expected.Payload) {
+		t.Fatalf("unexpected result: got %+v", result)
+	}
 }
