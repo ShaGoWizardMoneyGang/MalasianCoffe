ccd8dc3d6e3f852fddac727c7bb2cc0946ae4105
Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>
2025-09-27 13:53:25 -0300

    feat: gateway
    Signed-off-by: Tomas Fabrizio Orsi <tomas.orsi@lambdaclass.com>

---
+++ gateway/gateaway.go
@@ -0,0 +1,56 @@
+package gateway
+
+import (
+	"fmt"
+	// "net"
+
+	"os"
+
+	"net"
+
+	// "bufio"
+	"log/slog"
+
+	// "malasian_coffe/packet"
+	// "malasian_coffe/protocol"
+
+
+	"malasian_coffe/utils/uuid"
+)
+
+func main() {
+	// Directory with all dataset subdirectories
+	gateway_addr := os.Args[1]
+
+	list, err := net.Listen("tcp", gateway_addr)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create listener. Error: %s", err))
+	}
+	defer list.Close()
+
+	for {
+
+		// Wait for a connection.
+
+		conn, err := list.Accept()
+
+		if err != nil {
+
+			slog.Error("%s", err)
+
+		}
+
+		// Handle the connection in a new goroutine.
+
+		// The loop then returns to accepting, so that
+
+		// multiple connections may be served concurrently.
+
+		go handle_connection(conn)
+	}
+
+}
+
+func handle_connection(conn net.Conn) {
+	session_id := uuid.GenerateUUID()
+}
