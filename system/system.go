8e00478f229321797788b0958fc2c0f79b83aace
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-09-28 10:04:24 -0300

    feat: uso crudo de rabbit de ejemplo 🍑
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>
    Co-authored-by: Bruno Starnone <bstarnone@fi.uba.ar>
    Co-authored-by: mariana-jg <mjuarezg@fi.uba.ar>

--- system/system.go
+++ system/system.go
@@ -9,9 +9,34 @@ import (
 	"malasian_coffe/packet"
 
 	"malasian_coffe/utils/network"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	rconn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		panic(fmt.Errorf(`failed to rconnect to RabbitMQ: %w. Is the daemon active?
+		Try running:
+
+		sudo systemctl start rabbitmq
+		or
+		sudo rc-service rabbitmq start`))
+	}
+	ch, err := rconn.Channel()
+	if err != nil {
+		panic(fmt.Errorf("Failed to connect to the channel: %w", err))
+	}
+	ch.QueueDeclare(
+		"prueba",  // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+
 	rabbit_addr := os.Args[1]
 
 	list, err := net.Listen("tcp", rabbit_addr)
@@ -31,5 +56,11 @@ func main() {
 			panic(err)
 		}
 		fmt.Printf("%v\n", packet)
+		ch.Publish("", "prueba", false, false,
+		amqp.Publishing{
+			// DeliveryMode: amqp.Persistent,
+			ContentType: "text/plain",
+			Body:        packet.Serialize(),
+		})
 	}
 }
