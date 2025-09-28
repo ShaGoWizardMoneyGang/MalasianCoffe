ac3f2eb5245850733761c959fe857a483cdee43d
mariana-jg <mjuarezg@fi.uba.ar>
2025-09-27 23:38:17 -0300

    Refactor on counter with switch structure

--- system/counter/counter.go
+++ system/counter/counter.go
@@ -7,63 +7,59 @@ import (
 	"strings"
 )
 
-type counterOptions func(string) string
+// Recibe transaction_id, store_id, user_id
+// Devuelve user_id,store_id,cantidad por línea, ordenado por store_id y luego user_id para mantenerlo determinístico
+func (c *Counter) countFunctionQuery4(input string) string {
+	rows := strings.Split(input, "\n")
+	rows = rows[:len(rows)-1] // El split me genera 1 linea de mas vacia por el ultimo /n, la ignoro
 
-var (
-	// Recibe transaction_id, store_id, user_id
-	// Devuelve user_id,store_id,cantidad por línea, ordenado por store_id y luego user_id para mantenerlo determinístico
-	countFunctionQuery4 counterOptions = func(input string) string {
-		rows := strings.Split(input, "\n")
-		rows = rows[:len(rows)-1] // El split me genera 1 linea de mas vacia por el ultimo /n, la ignoro
-
-		type key struct {
-			store string
-			user  string
-		}
-		// tengo un map donde voy contando las apariciones por store y user
-		counts := map[key]int{}
-
-		for _, r := range rows {
-
-			cols := strings.Split(r, ",") // este comportamiento podria modularizarlo para todos
-			if len(cols) < 3 {
-				panic("Invalid data format")
-			}
-
-			storeID := cols[1]
-			userID := cols[2]
-			counts[key{store: storeID, user: userID}]++
-		}
-
-		// ordeno, esto podria modularizarlo???
-		keys := make([]key, 0, len(counts))
-		for k := range counts {
-			keys = append(keys, k)
-		}
-		sort.Slice(keys, func(i, j int) bool {
-			if keys[i].store == keys[j].store {
-				return keys[i].user < keys[j].user
-			}
-			return keys[i].store < keys[j].store
-		})
-
-		var b strings.Builder
-		for _, k := range keys {
-			fmt.Fprintf(&b, "%s,%s,%d\n", k.user, k.store, counts[k])
-		}
-		return b.String()
+	type key struct {
+		store string
+		user  string
 	}
-)
+	// tengo un map donde voy contando las apariciones por store y user
+	counts := map[key]int{}
 
-type Counter struct {
-	Function counterOptions
+	for _, r := range rows {
+
+		cols := strings.Split(r, ",") // este comportamiento podria modularizarlo para todos
+		if len(cols) < 3 {
+			panic("Invalid data format")
+		}
+
+		storeID := cols[1]
+		userID := cols[2]
+		counts[key{store: storeID, user: userID}]++
+	}
+
+	// ordeno, esto podria modularizarlo???
+	keys := make([]key, 0, len(counts))
+	for k := range counts {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool {
+		if keys[i].store == keys[j].store {
+			return keys[i].user < keys[j].user
+		}
+		return keys[i].store < keys[j].store
+	})
+
+	var b strings.Builder
+	for _, k := range keys {
+		fmt.Fprintf(&b, "%s,%s,%d\n", k.user, k.store, counts[k])
+	}
+	return b.String()
 }
 
-func (c *Counter) Process(pkt packet.Packet) packet.Packet {
-	returned := packet.Packet{Payload: []byte("")}
-	if c.Function != nil {
-		res := c.Function(string(pkt.Payload))
-		returned.Payload = []byte(res)
-	}
-	return returned
+type Counter struct {
+}
+
+func (c *Counter) Process(pkt packet.Packet) []packet.Packet {
+	// unica funcion de counter, va directo
+	input := pkt.GetPayload()
+	output := c.countFunctionQuery4(input)
+	outputs := []string{output}
+	newPacket := packet.ChangePayload(pkt, outputs)
+
+	return newPacket
 }
--- system/counter/counter_test.go
+++ system/counter/counter_test.go
@@ -7,23 +7,31 @@ import (
 
 func TestCountByUserAndStore(t *testing.T) {
 	// transaction_id,store_id,user_id
-	input := "2ae6d188-76c2-4095-b861-ab97d3cd9312,4,1038745.0\n" +
+	input := "" +
+		"2ae6d188-76c2-4095-b861-ab97d3cd9312,4,1038745.0\n" +
 		"7d0a474d-62f4-442a-96b6-a5df2bda8832,7,3296.0\n" +
 		"85f86fef-fddb-4eef-9dc3-1444553e6108,4,1038745.0\n" +
 		"4c41d179-f809-4d5a-a5d7-acb25ae1fe98,4,838764.0\n" +
 		"51e44c8e-4812-4a15-a9f9-9a46b62424d6,7,3296.0\n"
 
-	pkt := packet.Packet{Payload: []byte(input)}
-	worker := &Counter{
-		Function: countFunctionQuery4,
+	pkt := packet.Packet{}
+	pkt = packet.ChangePayload(pkt, []string{input})[0]
+
+	worker := &Counter{}
+	outs := worker.Process(pkt)
+	if len(outs) == 0 {
+		t.Fatalf("Process devolvió 0 paquetes")
 	}
 
-	contados := string(worker.Process(pkt).Payload)
+	got := outs[0].GetPayload()
 
-	// orden con store asc y por user asc
-	esperados := "1038745.0,4,2\n838764.0,4,1\n3296.0,7,2\n"
+	// orden: store asc y luego user asc
+	want := "" +
+		"1038745.0,4,2\n" +
+		"838764.0,4,1\n" +
+		"3296.0,7,2\n"
 
-	if contados != esperados {
-		panic("No se obtuvo la salida esperada")
+	if got != want {
+		t.Fatalf("salida inesperada.\nwant:\n%q\ngot:\n%q", want, got)
 	}
 }
