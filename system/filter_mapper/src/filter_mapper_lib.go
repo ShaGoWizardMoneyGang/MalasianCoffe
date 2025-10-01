85f0d818306b1bb9f7bf22c621dd8612a307a097
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-10-01 15:14:39 -0300

    feat: user filter mapper
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- system/filter_mapper/src/filter_mapper_lib.go
+++ system/filter_mapper/src/filter_mapper_lib.go
@@ -164,6 +164,10 @@ func filterFunctionQuery4UsersBirthdates(input string) string {
 		if len(data) < 4 {
 			panic("Invalid data format")
 		}
+		if data[0]  == "" || data[2] == "" {
+			slog.Debug("Registro con menos de 9 columnas, dropeado")
+			continue
+		}
 		final += data[0] + "," + data[2] + "\n"
 	}
 	return final
@@ -306,6 +310,35 @@ func (sfm *StoreFilterMapper) Process(pkt packet.Packet) []OutBoundMessage {
 
 // ============================== StoreFilter =================================
 
+// =============================== UserFilter ==================================
+type UserFilterMapper struct {
+	colaSalida4 *middleware.MessageMiddlewareQueue
+}
+
+func (ufm *UserFilterMapper) Build(rabbitAddr string) {
+	colaSalida4 := instanceQueue("FilteredUsers4", rabbitAddr)
+
+	ufm.colaSalida4  = colaSalida4
+}
+
+func (ufm *UserFilterMapper) Process(pkt packet.Packet) []OutBoundMessage {
+	input := pkt.GetPayload()
+
+	// Ambas payloads iguales
+	filtered_users := []string{filterFunctionQuery4UsersBirthdates(input)}
+
+	newPayload := packet.ChangePayload(pkt, filtered_users)
+	outBoundMessage := []OutBoundMessage{
+		{
+			Packet: newPayload[0],
+			ColaSalida: ufm.colaSalida4,
+		},
+	}
+
+	return outBoundMessage
+}
+
+// =============================== UserFilter ==================================
 type FilterMapper interface {
 	// Funcio que hace el filtrado
 	Process(pkt packet.Packet) []OutBoundMessage
@@ -320,6 +353,8 @@ func FilterMapperBuilder(datasetName string, rabbitAddr string) FilterMapper {
 		filterMapper = &TransactionFilterMapper{}
 	case "store":
 		filterMapper = &StoreFilterMapper{}
+	case "users":
+		filterMapper = &UserFilterMapper{}
 	default:
 		panic(fmt.Sprintf("Unknown 'dataset' %s", datasetName))
 	}
