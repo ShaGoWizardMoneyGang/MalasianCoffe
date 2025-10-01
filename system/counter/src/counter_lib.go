50d7f349f1e1e307b2b9ee403efda9213d186520
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-10-01 15:36:16 -0300

    refactor: move counter_lib to counter/src/
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- system/counter/src/counter_lib.go
+++ system/counter/src/counter_lib.go
@@ -3,13 +3,14 @@ package counter
 import (
 	"fmt"
 	"malasian_coffe/packets/packet"
-	"sort"
+	"malasian_coffe/system/middleware"
 	"strings"
 )
 
+// NOTE: Gracias Mari por anadir estas lineas de input output
 // Recibe transaction_id, store_id, user_id
 // Devuelve user_id,store_id,cantidad por línea, ordenado por store_id y luego user_id para mantenerlo determinístico
-func (c *Counter) countFunctionQuery4(input string) string {
+func (c *CounterQuery4) countFunctionQuery4(input string) string {
 	rows := strings.Split(input, "\n")
 	rows = rows[:len(rows)-1] // El split me genera 1 linea de mas vacia por el ultimo /n, la ignoro
 
@@ -27,6 +28,7 @@ func (c *Counter) countFunctionQuery4(input string) string {
 			panic("Invalid data format")
 		}
 
+		// NOTE: Estos no deberian estar vacios. Si lo estan, buscar error en el filter mapper
 		storeID := cols[1]
 		userID := cols[2]
 		counts[key{store: storeID, user: userID}]++
@@ -37,12 +39,13 @@ func (c *Counter) countFunctionQuery4(input string) string {
 	for k := range counts {
 		keys = append(keys, k)
 	}
-	sort.Slice(keys, func(i, j int) bool {
-		if keys[i].store == keys[j].store {
-			return keys[i].user < keys[j].user
-		}
-		return keys[i].store < keys[j].store
-	})
+	// NOTE: Quito el sort para reducir el costo. Para mantener determinisitco, anadir sort al resultado en los tests
+	// sort.Slice(keys, func(i, j int) bool {
+	// 	if keys[i].store == keys[j].store {
+	// 		return keys[i].user < keys[j].user
+	// 	}
+	// 	return keys[i].store < keys[j].store
+	// })
 
 	var b strings.Builder
 	for _, k := range keys {
@@ -51,9 +54,30 @@ func (c *Counter) countFunctionQuery4(input string) string {
 	return b.String()
 }
 
-type Counter struct {
+// ============================= CounterQuery4 ================================
+
+// ============================= CounterQuery4 ================================
+
+type CounterQuery4 struct {
+	colaCountedUsers4 *middleware.MessageMiddlewareQueue
 }
 
+func (c *CounterQuery4) Process(pkt packet.Packet) []packet.OutBoundMessage {
+	input := pkt.GetPayload()
+
+	counted_result := []string{c.countFunctionQuery4(input)}
+
+	newPayload := packet.ChangePayload(pkt, counted_result)
+
+	outBoundMessage := []packet.OutBoundMessage{
+		{
+			Packet: newPayload[0],
+			ColaSalida: c.colaCountedUsers4,
+		},
+	}
+
+	return outBoundMessage
+}
 type CounterInt interface {
 	// Funcio que hace el filtrado
 	Process(pkt packet.Packet) []packet.OutBoundMessage
@@ -61,7 +85,7 @@ type CounterInt interface {
 	Build(rabbitAddr string)
 }
 
-func (c *Counter) Process(pkt packet.Packet) []packet.Packet {
+func (c *CounterQuery4) Process(pkt packet.Packet) []packet.Packet {
 	// unica funcion de counter, va directo
 	input := pkt.GetPayload()
 	output := c.countFunctionQuery4(input)
