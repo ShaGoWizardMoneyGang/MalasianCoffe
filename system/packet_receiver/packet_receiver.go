be431a310e9f04e44f120290d62abb9b8cfaea77
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-10-04 19:35:37 -0300

    feat: (WIP) packet builder
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- system/packet_receiver/packet_receiver.go
+++ system/packet_receiver/packet_receiver.go
@@ -1,1 +1,31 @@
 package packetreceiver
+
+import (
+	"log/slog"
+	"malasian_coffe/packets/packet"
+)
+
+type PacketReceiver struct {
+	received_packages map[string]packet.Packet
+
+	receivedEOF bool
+}
+
+func NewPacketReceiver() PacketReceiver {
+	return PacketReceiver{
+		received_packages: make(map[string]packet.Packet),
+		receivedEOF: false,
+	}
+}
+
+// Devuelve un booleano que representa si el packete se recibio en su
+// totalidad o no
+func (pr *PacketReceiver) ReceivePackt(pkt packet.Packet) {
+	uuid := pkt.GetSessionID()
+
+	_, exits := pr.received_packages[uuid]
+	if exits {
+		slog.Debug("Duplicate packet received")
+	}
+	pr.received_packages[uuid] = pkt
+}
