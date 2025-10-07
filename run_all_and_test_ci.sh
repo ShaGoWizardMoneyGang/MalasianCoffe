07efdb1d3815bb50890006b820f29dbe55995448
Bruno Starnone <bstarnone@fi.uba.ar>
2025-10-07 00:02:51 -0300

    a ver si ahora sí anda final final

--- run_all_and_test_ci.sh
+++ run_all_and_test_ci.sh
@@ -3,4 +3,11 @@ make build
 
 docker compose -f docker-compose-ci.yml up -d
 
+while ! docker ps -q -f name=client1 | grep -q .; do
+  echo "Esperando a que client1 se cree..."
+  sleep 1
+done
+
 docker wait client1
+
+bash test_outputs.sh RED
