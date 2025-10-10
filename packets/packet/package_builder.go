b28101cfdf692984cdbd89cb4067c58778e73120
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-10-09 21:00:43 -0300

    Revert "fix: close gateway connection"
    This reverts commit 5719ccb2462ea82cb7611b8f2d4c31740115b9e9.

--- packets/packet/package_builder.go
+++ packets/packet/package_builder.go
@@ -88,8 +88,6 @@ func (pb *PacketBuilder) Send(register string) error {
 }
 
 func (pb *PacketBuilder) End() error {
-	defer pb.gatewayIP.Close()
-
 	packet, err := pb.createPacket(pb.payload_buffer.String(), true)
 
 	if err != nil {
@@ -104,7 +102,6 @@ func (pb *PacketBuilder) End() error {
 	pb.payload_buffer.Reset()
 
 	bitacora.Info(fmt.Sprintf("Envie %d paquetes, del dataset %s", pb.currentSequenceNumber, pb.directory_name))
-
 
 	return nil
 }
