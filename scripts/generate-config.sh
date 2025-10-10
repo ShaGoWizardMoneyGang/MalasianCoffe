35506bd3107126cd6341023c5ce651d5169b2a0f
Tomas Fabrizio Orsi <torsi@fi.uba.ar>
2025-10-10 01:43:53 -0300

    feat: add multiple dockers to CI
    Signed-off-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

---
+++ scripts/generate-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+config=$1
+
+estados() {
+    echo "Las opciones disponibles son:"
+    echo "- '1DeCada': 1 worker de cada tipo"
+    echo "- 'MuchosSinEstado': Varios workers de los que no tienen estado"
+}
+
+if [ -z ${config} ]; then
+    echo "No se detecto ningun config deseado"
+    estados
+elif [ ${config} = "1DeCada" ]; then
+    cp -f .github/data/compose1DeCada.config compose.config
+elif [ ${config} = "MuchosSinEstado" ]; then
+    # Viva la anarquia ✊
+    # https://es.wikipedia.org/wiki/Anarqu%C3%ADa
+    cp -f .github/data/composeMuchosSinEstado.config compose.config
+else
+    echo "Opcion '${config}' no reconocida"
+    estados
+    exit 1
+fi
