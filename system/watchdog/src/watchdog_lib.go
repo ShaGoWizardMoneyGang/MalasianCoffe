057e599e58e0447e6a522b15c511d0ed596f25ec
Bruno Starnone <bstarnone@fi.uba.ar>
2025-11-23 13:00:35 -0300

    WIP-refactor: unificar anillo y heartbeats. Status: se pasa un mensaje por todo el anillo y vuelve al principal

--- system/watchdog/src/watchdog_lib.go
+++ system/watchdog/src/watchdog_lib.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 )
 
 const (
@@ -16,81 +15,6 @@ const (
 
 type WatchdogListener struct {
 	conn net.UDPConn
-}
-
-// Lógica de comunicación Master-Réplicas
-/*
-Si el watchdog es réplica:
-- Espera los heartbeats del lider, si no llegan avisa
-Si el watchdog es el líder:
-- Lee el archivo de sheeps.txt, guarda los servicios, etc
-- Define lista de réplicas (a parametrizar)
-- Arranca una go routine que periódicamente envia por UDP "Heartbeat" a cada réplica
-usando HeartbeatLoop()
-
-Después todo sigue practicamente igual a lo que ya teníamos
-*/
-
-// ============================= HEARTBEAT ================================
-
-// Envía heartbeat
-func SendHeartbeat(replicaAddr string) {
-	conn, err := net.Dial("udp", replicaAddr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error al conectar con replica %s: %v\n", replicaAddr, err)
-		return
-	}
-	defer conn.Close()
-	conn.Write([]byte("HEARTBEAT"))
-}
-
-// Envía heartbeats periódicos a todas las réplicas
-func HeartbeatLoop(replicaList []string) {
-	// LOCURA LO QUE ES ESTE TICKER
-	// El ticker es un objeto que genera eventos cada cierto intervalo
-	ticker := time.NewTicker(HEARTBEAT_PERIOD * time.Second)
-	defer ticker.Stop()
-	// este bucle espera a que pase el intervalo del ticker
-	// cada vez que pase el tiempo, el canal ticker.c recibe un valor y el codigo dentro del bucle se ejecuta
-	for {
-		<-ticker.C
-		for _, replica := range replicaList {
-			addr := replica + ":" + fmt.Sprint(HEARTBEAT_PORT)
-			SendHeartbeat(addr)
-		}
-	}
-}
-
-// ============================= REPLICA ================================
-
-// Espera los heartbeats del lider, si no llegan avisa
-func ReplicaHeartbeatLoop() {
-	addr := net.UDPAddr{
-		Port: HEARTBEAT_PORT,
-		IP:   net.ParseIP("0.0.0.0"),
-	}
-	// Abro socket
-	conn, err := net.ListenUDP("udp", &addr)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-	buffer := make([]byte, 1024)
-	// Guardo cuándo llegó el último heartbeat
-	lastHeartbeat := time.Now()
-	for {
-		// Timeout para la lectura, si nada llega en HEARTBEAT_TIMEOUT falla
-		conn.SetReadDeadline(time.Now().Add(HEARTBEAT_TIMEOUT * time.Second))
-		n, _, err := conn.ReadFromUDP(buffer)
-		// si recibi mensjae y es "HEARTBEAT", actualizo el tiempo del último heartbeat
-		if err == nil && string(buffer[:n]) == "HEARTBEAT" {
-			lastHeartbeat = time.Now()
-			fmt.Println("Heartbeat recibido del líder")
-			// si no recibe nada en ese tiempo, avisa que no llegó
-		} else if time.Since(lastHeartbeat) > HEARTBEAT_TIMEOUT*time.Second {
-			fmt.Println("No recibí heartbeat del líder en el tiempo esperado")
-		}
-	}
 }
 
 // ============================= USED BY WORKER ================================
--- system/watchdog/watchdog.go
+++ system/watchdog/watchdog.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	watchdog "malasian_coffe/system/watchdog/src"
 	"malasian_coffe/utils/ring"
-	"net"
 	"os"
 	"os/exec"
-	"strings"
 	"time"
 )
 
@@ -40,10 +37,10 @@ func restartContainer(name string) {
 
 func main() {
 	fmt.Println("Esperando a que arranque el sistema...")
-	time.Sleep(10 * time.Second)
+	time.Sleep(5 * time.Second)
 
 	// PRUEBA DE ANILLO
-	myName, err := os.Hostname()
+	myName, err := os.Hostname() //watchdog_1, watchdog_2, ...
 	if err != nil {
 		panic("No se pudo obtener el hostname")
 	}
@@ -55,108 +52,116 @@ func main() {
 	neighbor := ring.GetNeighbor(myID, members)
 	fmt.Printf("Soy %s, mi vecino en el anillo es %s\n", myName, neighbor)
 
-	go ring.ListenRing(ring.WatchdogNode{ID: myID, Addr: myName, Neighbor: neighbor})
-	time.Sleep(2 * time.Second)
-	ring.SendHello(ring.WatchdogNode{ID: myID, Addr: myName, Neighbor: neighbor})
-	// PARA PRUEBA LOS SLEEPS E
-	time.Sleep(10 * time.Second)
+	ringNode := ring.WatchdogNode{ID: myID, Addr: myName, Neighbor: neighbor}
+	go ring.ListenRing(ringNode)
 
-	amILeader := os.Args[1]
-	if amILeader != "LEADER" {
+	amIStarter := os.Args[1]
+	if amIStarter != "STARTER" {
 		// Es réplica
-		fmt.Println("Hola soy una réplica, escucho heartbeats del líder")
-		watchdog.ReplicaHeartbeatLoop() // loop para escuchar heartbeats
+		// fmt.Println("Hola soy una réplica, escucho heartbeats del líder")
+		// ring.ReplicaHeartbeatLoop() // loop para escuchar heartbeats
+		//si esto retorna es porque algo se rompió
+		select {}
 		return
 	}
-	fmt.Println("Soy el líder, comienzo watchdog")
-	file, err := os.ReadFile(SHEEPS_FILE)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "No se pudo abrir el archivo %s: %v\n", SHEEPS_FILE, err)
-	}
-
-	listOfServices := strings.Split(string(file), "\n")
-	services := make([]string, 0, len(listOfServices))
-	for _, s := range listOfServices {
-		s = strings.TrimSpace(s)
-		if s != "" {
-			services = append(services, s)
-			fmt.Println("Servicio:", s)
-		}
-	}
-
-	// Lista de réplicas harcodeada con 2
-	// TODO: parametrizar
-	// replicaList := []string{"watchdog_2", "watchdog_3"}
-
-	file, err = os.ReadFile(PUPPIES_FILE)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "No se pudo abrir el archivo %s: %v\n", PUPPIES_FILE, err)
-	}
-
-	listOfPuppies := strings.Split(string(file), "\n")
-	members = make([]string, 0, len(listOfPuppies))
-	for _, p := range listOfPuppies {
-		p = strings.TrimSpace(p)
-		if p != "" {
-			members = append(members, p)
-			fmt.Println("Réplicas:", p)
-		}
-	}
-
-	fmt.Println(members)
-
-	go watchdog.HeartbeatLoop(members)
-
-	addr := net.UDPAddr{
-		Port: watchdog.HEALTHCHECK_PORT,
-		IP:   net.ParseIP("0.0.0.0"),
-	}
-	connListen, err := net.ListenUDP("udp", &addr)
-	if err != nil {
-		panic(err)
-	}
-	defer connListen.Close()
-
-	buffer := make([]byte, 1024)
+	i := 0
 	for {
-		for _, serviceName := range services {
-			healthCheckAddress := serviceName + ":" + fmt.Sprint(watchdog.HEALTHCHECK_PORT)
-			successfulHealthcheck := false
-			for attempt := 1; attempt <= MAX_RETRIES; attempt++ {
-				fmt.Printf("Intento %d de %d: enviando PING a %s\n", attempt, MAX_RETRIES, healthCheckAddress)
-				// Mando PING
-				err := watchdog.Ping(healthCheckAddress)
-				if err != nil {
-					fmt.Fprintf(os.Stderr, "Error enviando ping a %s: %v\n", healthCheckAddress, err)
-				} else {
-					// Acá con SetReadDeadline defino que después de la hora actual + 2 segundos vence el timeout
-					connListen.SetReadDeadline(time.Now().Add(TIMEOUT * time.Second))
-					n, _, err := connListen.ReadFromUDP(buffer)
-					// Si el Read NO devuelve error, entonces se recibió el PONG y salimos
-					if err == nil {
-						fmt.Printf("Watchdog recibió PONG del %s: %s\n", serviceName, string(buffer[:n]))
-						successfulHealthcheck = true
-						break
-					}
-					// Si el error es de network y es un timeout, el PONG No llegó en el tiempo establecido (2seg)
-					netError, isNetError := err.(net.Error)
-					if isNetError == true && netError.Timeout() {
-						fmt.Printf("No se recibió PONG en %v segundos\n ", TIMEOUT)
-					}
-				}
-				// Espero un poco antes del siguiente intento
-				time.Sleep(1 * time.Second)
-			}
-
-			if successfulHealthcheck == false {
-				fmt.Printf("No se recibió PONG tras %d intentos. Reiniciando %s\n", MAX_RETRIES, serviceName)
-				restartContainer(serviceName)
-			} else {
-				fmt.Println("El servicio respondió correctamente, no hace falta reiniciar")
-			}
+		time.Sleep(2 * time.Second)
+		ring.SendHello(ringNode)
+		i++
+		if i > 2 {
+			return
 		}
-		time.Sleep(watchdog.HEARTBEAT_PERIOD * time.Second)
 	}
+	// go ring.HeartbeatLoop(members)
+
+	fmt.Println("Soy el líder, comienzo watchdog")
+	// file, err := os.ReadFile(SHEEPS_FILE)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "No se pudo abrir el archivo %s: %v\n", SHEEPS_FILE, err)
+	// }
+
+	// listOfServices := strings.Split(string(file), "\n")
+	// services := make([]string, 0, len(listOfServices))
+	// for _, s := range listOfServices {
+	// 	s = strings.TrimSpace(s)
+	// 	if s != "" {
+	// 		services = append(services, s)
+	// 		fmt.Println("Servicio:", s)
+	// 	}
+	// }
+
+	// // Lista de réplicas harcodeada con 2
+	// // TODO: parametrizar
+	// // replicaList := []string{"watchdog_2", "watchdog_3"}
+
+	// file, err = os.ReadFile(PUPPIES_FILE)
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "No se pudo abrir el archivo %s: %v\n", PUPPIES_FILE, err)
+	// }
+
+	// listOfPuppies := strings.Split(string(file), "\n")
+	// members = make([]string, 0, len(listOfPuppies))
+	// for _, p := range listOfPuppies {
+	// 	p = strings.TrimSpace(p)
+	// 	if p != "" {
+	// 		members = append(members, p)
+	// 		fmt.Println("Réplicas:", p)
+	// 	}
+	// }
+
+	// fmt.Println(members)
+
+	// addr := net.UDPAddr{
+	// 	Port: ring.HEALTHCHECK_PORT,
+	// 	IP:   net.ParseIP("0.0.0.0"),
+	// }
+	// connListen, err := net.ListenUDP("udp", &addr)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer connListen.Close()
+
+	// buffer := make([]byte, 1024)
+	// for {
+	// 	for _, serviceName := range services {
+	// 		healthCheckAddress := serviceName + ":" + fmt.Sprint(watchdog.HEALTHCHECK_PORT)
+	// 		successfulHealthcheck := false
+	// 		for attempt := 1; attempt <= MAX_RETRIES; attempt++ {
+	// 			fmt.Printf("Intento %d de %d: enviando PING a %s\n", attempt, MAX_RETRIES, healthCheckAddress)
+	// 			// Mando PING
+	// 			err := watchdog.Ping(healthCheckAddress)
+	// 			if err != nil {
+	// 				fmt.Fprintf(os.Stderr, "Error enviando ping a %s: %v\n", healthCheckAddress, err)
+	// 			} else {
+	// 				// Acá con SetReadDeadline defino que después de la hora actual + 2 segundos vence el timeout
+	// 				connListen.SetReadDeadline(time.Now().Add(TIMEOUT * time.Second))
+	// 				n, _, err := connListen.ReadFromUDP(buffer)
+	// 				// Si el Read NO devuelve error, entonces se recibió el PONG y salimos
+	// 				if err == nil {
+	// 					fmt.Printf("Watchdog recibió PONG del %s: %s\n", serviceName, string(buffer[:n]))
+	// 					successfulHealthcheck = true
+	// 					break
+	// 				}
+	// 				// Si el error es de network y es un timeout, el PONG No llegó en el tiempo establecido (2seg)
+	// 				netError, isNetError := err.(net.Error)
+	// 				if isNetError == true && netError.Timeout() {
+	// 					fmt.Printf("No se recibió PONG en %v segundos\n ", TIMEOUT)
+	// 				}
+	// 			}
+	// 			// Espero un poco antes del siguiente intento
+	// 			time.Sleep(1 * time.Second)
+	// 		}
+
+	// 		if successfulHealthcheck == false {
+	// 			fmt.Printf("No se recibió PONG tras %d intentos. Reiniciando %s\n", MAX_RETRIES, serviceName)
+	// 			restartContainer(serviceName)
+	// 		} else {
+	// 			fmt.Println("El servicio respondió correctamente, no hace falta reiniciar")
+	// 		}
+	// 	}
+	// 	time.Sleep(watchdog.HEARTBEAT_PERIOD * time.Second)
+	// }
 }
 
 /*
