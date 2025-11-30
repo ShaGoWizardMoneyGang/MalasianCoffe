c7919ce5979931941439313d32c3e322b555f4ca
mariana-jg <mjuarezg@fi.uba.ar>
2025-11-30 20:22:49 -0300

    feat: persistencia en global aggregator 4

--- system/global_aggregator/src/globalAggregatorQuery4.go
+++ system/global_aggregator/src/globalAggregatorQuery4.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 
 	"malasian_coffe/packets/packet"
-	"malasian_coffe/packets/packet_receiver"
+	"malasian_coffe/packets/single_packet_receiver"
 	"malasian_coffe/system/middleware"
 	sessionhandler "malasian_coffe/system/session_handler"
 	watchdog "malasian_coffe/system/watchdog/src"
@@ -38,10 +38,95 @@ func (g *aggregator4Global) Build(rabbitAddr string, routing_key string, outs ma
 	g.sessionHandler = sessionhandler.NewSessionHandler(aggregateQuery4, g.outputChannel)
 }
 
-func updateAccumulator(consolidatedInput string, localAcc map[string]map[string]uint64) {
-	lines := strings.Split(consolidatedInput, "\n")
-	lines = lines[:len(lines)-1]
+func aggregate_4_func(accumulated_input string, new_input string) string {
+	localAcc := make(map[string]map[string]uint64)
 
+	if accumulated_input != "" {
+		lines := strings.Split(accumulated_input, "\n")
+		for _, line := range lines {
+			if line == "" {
+				continue
+			}
+			columns := strings.Split(line, ",")
+			if len(columns) != 3 {
+				continue
+			}
+			userID := columns[0]
+			storeID := columns[1]
+			amountStr := columns[2]
+
+			amount, err := strconv.ParseUint(amountStr, 10, 64)
+			if err != nil {
+				continue
+			}
+
+			if localAcc[storeID] == nil {
+				localAcc[storeID] = make(map[string]uint64)
+			}
+			localAcc[storeID][userID] = amount
+		}
+	}
+
+	if new_input != "" {
+		lines := strings.Split(new_input, "\n")
+		for _, line := range lines {
+			if line == "" {
+				continue
+			}
+			columns := strings.Split(line, ",")
+			if len(columns) != 3 {
+				continue
+			}
+			userID := columns[0]
+			storeID := columns[1]
+			amountStr := columns[2]
+
+			amount, err := strconv.ParseUint(amountStr, 10, 64)
+			if err != nil {
+				continue
+			}
+
+			if localAcc[storeID] == nil {
+				localAcc[storeID] = make(map[string]uint64)
+			}
+			localAcc[storeID][userID] += amount
+		}
+	}
+
+	var b strings.Builder
+	for storeID, users := range localAcc {
+		for userID, amount := range users {
+			fmt.Fprintf(&b, "%s,%s,%d\n", userID, storeID, amount)
+		}
+	}
+
+	return b.String()
+}
+
+func aggregateQuery4(sessionID string, inputChannel <-chan colas.PacketMessage, outputChannel chan<- packet.Packet) {
+	localReceiver := single_packet_receiver.NewSinglePacketReceiver(sessionID, aggregate_4_func)
+
+	var last_packet packet.Packet
+	var aggregated_packets string
+
+	for {
+		pktMsg := <-inputChannel
+		pkt := pktMsg.Packet
+
+		received_all := localReceiver.ReceivePacket(pktMsg)
+
+		if !received_all {
+			continue
+		}
+
+		aggregated_packets = localReceiver.GetPayload()
+		last_packet = pkt
+		break
+	}
+
+	totalAcc := make(map[string]map[string]uint64)
+
+	lines := strings.Split(aggregated_packets, "\n")
 	for _, line := range lines {
 		if line == "" {
 			continue
@@ -59,23 +144,21 @@ func updateAccumulator(consolidatedInput string, localAcc map[string]map[string]
 			panic("Amount con formato inválido")
 		}
 
-		if localAcc[storeID] == nil {
-			localAcc[storeID] = make(map[string]uint64)
+		if totalAcc[storeID] == nil {
+			totalAcc[storeID] = make(map[string]uint64)
 		}
-		localAcc[storeID][userID] += amount
+		totalAcc[storeID][userID] = amount
 	}
-}
 
-func buildOutput(localAcc map[string]map[string]uint64) string {
 	var b strings.Builder
-	stores := make([]string, 0, len(localAcc))
-	for store := range localAcc {
+	stores := make([]string, 0, len(totalAcc))
+	for store := range totalAcc {
 		stores = append(stores, store)
 	}
 	sort.Strings(stores)
 
 	for _, store := range stores {
-		users := localAcc[store]
+		users := totalAcc[store]
 
 		type UserAmount struct {
 			user   string
@@ -98,36 +181,14 @@ func buildOutput(localAcc map[string]map[string]uint64) string {
 			fmt.Fprintf(&b, "%s,%s\n", store, sortedSlice[i].user)
 		}
 	}
-	return b.String()
-}
 
-func aggregateQuery4(sessionID string, inputChannel <-chan colas.PacketMessage, outputChannel chan<- packet.Packet) {
-	localReceiver := packet_receiver.NewPacketReceiver("agregador-global-4")
-	localAcc := make(map[string]map[string]uint64)
-
-	var last_packet packet.Packet
-
-	for {
-		pktMsg := <-inputChannel
-		pkt := pktMsg.Packet
-
-		localReceiver.ReceivePacket(pktMsg)
-
-		if localReceiver.ReceivedAll() {
-			last_packet = pkt
-			break
-		}
-	}
-
-	consolidatedInput := localReceiver.GetPayload()
-
-	updateAccumulator(consolidatedInput, localAcc)
-
-	output := buildOutput(localAcc)
+	output := b.String()
 
 	newPkts := packet.ChangePayloadGlobalAggregator(last_packet, "transactions", []string{output})
+
 	outputChannel <- newPkts[0]
 
+	localReceiver.Clean()
 }
 
 func (g *aggregator4Global) Process() {
